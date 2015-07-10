f706bd975c6065ff691bf9eea90bd6cb1f7d3ffa
Alberto Delgado <albertodelcru@gmail.com>
2015-07-10 21:12:40 +0200

    Added gen items and destroy items, change folders organization

---
+++ Assets/Scripts/GenItems.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenItems : MonoBehaviour
+{
+    public GameObject[] objs = new GameObject[2];
+    private int num = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        objs[0] = (GameObject)Resources.Load("sfdfsdfsd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenItem();
+
+    }
+
+    void GenItem()
+    {
+        num = Random.Range(0, objs.Length);
+        Instantiate(objs[num], transform.position, Quaternion.identity);
+    }
+
+
+
+}
