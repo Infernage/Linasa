8a4cd9ee212be03006c677316019e9144f073b43
Alberto Castro Olea <er_maldito_diablo@hotmail.com>
2015-07-12 13:18:48 +0200

    Implemented items movement

---
+++ Assets/MoverItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverItem : MonoBehaviour
+{
+    public Vector3 direction;
+    public bool random;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        if (random) direction = new Vector3(Random.Range(-1F, 1F), Random.Range(-1F, 1F), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
