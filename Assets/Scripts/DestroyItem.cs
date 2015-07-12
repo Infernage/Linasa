2738120ba1763ff26ee419060f9ca6a960e74b52
Alberto Delgado <albertodelcru@gmail.com>
2015-07-12 15:20:28 +0200

    Fixed destructor

--- Assets/Scripts/DestroyItem.cs
+++ Assets/Scripts/DestroyItem.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroyItem : MonoBehaviour {
+public class DestroyItem : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //Debug.Log(transform.position.x + " " + transform.position.y);
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Destroy(col.gameObject);
-        GenItems.items--;
+        if (col.tag != "Destructor")
+        {
+            Destroy(col.gameObject);
+            GenItems.items--;
+        }
         //Debug.Log("Destroy trigger");
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        Destroy(col.gameObject);
-        GenItems.items--;
+        if (col.gameObject.tag != "Destructor")
+        {
+            Destroy(col.gameObject);
+            GenItems.items--;
+        }
         //Debug.Log("Destroy trigger");
     }
 }
